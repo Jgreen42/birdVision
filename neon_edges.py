26bc9325d7b445410f86e002119cca1dda4830c0
Jgreen42 <creativemind47@outlook.com>
2023-10-17 23:14:34 -0700

    abunch of changes but wont work troubleshooting

--- neon_edges.py
+++ neon_edges.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 from flask import Flask, render_template, Response
 from your_module import get_frames  # Import your frame processing function
 
@@ -18,9 +17,9 @@ def generate_frames():
 
         # Check if frames are not None
         if bird_vision_frame is not None and motion_detection_frame is not None:
-            # You may need to encode the frames to base64 and send them to the template
-            bird_vision_encoded = encode_image_to_base64(bird_vision_frame)
-            motion_detection_encoded = encode_image_to_base64(motion_detection_frame)
+            # Convert frames to JPEG format
+            bird_vision_encoded = cv2.imencode('.jpg', bird_vision_frame)[1].tobytes()
+            motion_detection_encoded = cv2.imencode('.jpg', motion_detection_frame)[1].tobytes()
 
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + bird_vision_encoded + b'\r\n\r\n')
@@ -35,34 +34,5 @@ def video_feed():
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
+if __name__ == '__main':
     app.run(debug=True)
-
-
-# import cv2
-# import numpy as np
-
-# def get_frames(frame):
-#     # Bird Vision
-#     uv_filter = frame.copy()
-#     uv_filter[:, :, 2] += uv_filter[:, :, 1]
-#     gray = cv2.cvtColor(uv_filter, cv2.COLOR_BGR2GRAY)
-#     edges = cv2.Canny(gray, 60, 150)
-#     edge_color = cv2.applyColorMap(edges, cv2.COLORMAP_JET)
-#     _, edge_mask = cv2.threshold(edges, 50, 255, cv2.THRESH_BINARY)
-#     non_edge_mask = cv2.bitwise_not(edge_mask)
-#     bird_vision_frame = cv2.bitwise_and(frame, frame, mask=non_edge_mask)
-#     bird_vision_frame += edge_color
-#     bird_vision_rgb = cv2.cvtColor(bird_vision_frame, cv2.COLOR_BGR2RGB)
-
-#     # Motion Detection
-#     motion_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     if get_frames.prev_frame is not None:
-#         frame_diff = cv2.absdiff(get_frames.prev_frame, motion_frame)
-#         _, frame_diff = cv2.threshold(frame_diff, 15, 255, cv2.THRESH_BINARY)
-#         return bird_vision_rgb, frame_diff
-
-#     get_frames.prev_frame = motion_frame
-#     return None, None
-
-# get_frames.prev_frame = None
--- static/script.js
+++ static/script.js
@@ -1,44 +1,97 @@
 const video = document.getElementById('webcam-feed');
-const birdVisionCanvas = document.getElementById('bird-vision-canvas');
-const motionDetectionCanvas = document.getElementById('motion-detection-canvas');
+const canvas = document.getElementById('output-canvas');
+const context = canvas.getContext('2d');
 
 navigator.mediaDevices
   .getUserMedia({ video: true })
   .then((stream) => {
     video.srcObject = stream;
-
-    const contextBirdVision = birdVisionCanvas.getContext('2d');
-    const contextMotionDetection = motionDetectionCanvas.getContext('2d');
-    
-    let prevFrame = null;
-
-    const processFrame = () => {
-      const width = video.videoWidth;
-      const height = video.videoHeight;
-
-      birdVisionCanvas.width = width;
-      birdVisionCanvas.height = height;
-      motionDetectionCanvas.width = width;
-      motionDetectionCanvas.height = height;
-
-      contextBirdVision.drawImage(video, 0, 0, width, height);
-      
-      // Implement bird vision effect here
-      
-      if (prevFrame) {
-        contextMotionDetection.drawImage(video, 0, 0, width, height);
-        const currentFrame = contextMotionDetection.getImageData(0, 0, width, height).data;
-        
-        // Implement motion detection logic here
-        
-        prevFrame = currentFrame;
-      }
-      
-      requestAnimationFrame(processFrame);
-    };
-
-    processFrame();
   })
   .catch((error) => {
     console.error('Error accessing webcam:', error);
   });
+
+video.addEventListener('play', () => {
+  const { videoWidth, videoHeight } = video;
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  const bufferCanvas = document.createElement('canvas');
+  bufferCanvas.width = videoWidth;
+  bufferCanvas.height = videoHeight;
+  const bufferContext = bufferCanvas.getContext('2d');
+
+  const processFrame = () => {
+    bufferContext.drawImage(video, 0, 0, videoWidth, videoHeight);
+    const frame = bufferContext.getImageData(0, 0, videoWidth, videoHeight);
+
+
+// Apply your bird vision effect here
+const birdVisionEffect = (frame) => {
+    const uv_filter = new cv.Mat();
+    cv.cvtColor(frame, uv_filter, cv.COLOR_BGR2BGRA);
+    const gray = new cv.Mat();
+    cv.cvtColor(uv_filter, gray, cv.COLOR_BGR2GRAY);
+    const edges = new cv.Mat();
+    cv.Canny(gray, edges, 60, 150);
+    const edge_color = new cv.Mat();
+    cv.applyColorMap(edges, edge_color, cv.COLORMAP_JET);
+    const edge_mask = new cv.Mat();
+    cv.threshold(edges, edge_mask, 50, 255, cv.THRESH_BINARY);
+    const non_edge_mask = new cv.Mat();
+    cv.bitwise_not(edge_mask, non_edge_mask);
+    const bird_vision_frame = new cv.Mat();
+    cv.bitwise_and(uv_filter, uv_filter, bird_vision_frame, non_edge_mask);
+    cv.add(bird_vision_frame, edge_color, bird_vision_frame);
+    cv.cvtColor(bird_vision_frame, bird_vision_frame, cv.COLOR_BGRA2BGR);
+  
+    uv_filter.delete(); // Don't forget to release Mats to avoid memory leaks
+    gray.delete();
+    edges.delete();
+    edge_color.delete();
+    edge_mask.delete();
+    non_edge_mask.delete();
+    return bird_vision_frame;
+  };
+  
+
+  // Apply your motion detection effect here
+const motionDetectionEffect = (frame, prev_frame) => {
+  const motion_frame = new cv.Mat();
+  cv.cvtColor(frame, motion_frame, cv.COLOR_BGR2GRAY);
+
+  if (prev_frame !== null) {
+    const frame_diff = new cv.Mat();
+    cv.absdiff(prev_frame, motion_frame, frame_diff);
+    cv.threshold(frame_diff, frame_diff, 15, 255, cv.THRESH_BINARY);
+
+    // You can further process frame_diff as needed
+    // E.g., display it, calculate motion metrics, etc.
+
+    frame_diff.delete(); // Release the Mat
+  }
+
+  return motion_frame;
+};
+
+
+    context.putImageData(frame, 0, 0);
+    requestAnimationFrame(processFrame);
+  };
+
+  processFrame();
+});
+
+const frame = bufferContext.getImageData(0, 0, videoWidth, videoHeight);
+
+// Apply bird vision effect
+const birdVisionFrame = birdVisionEffect(frame);
+
+// Apply motion detection effect
+const motionDetectionFrame = motionDetectionEffect(frame, prev_frame);
+
+// Update the canvas with the results
+context.putImageData(birdVisionFrame, 0, 0); // Display the Bird Vision effect
+// Additionally, you can display motionDetectionFrame if needed
+
+requestAnimationFrame(processFrame);
