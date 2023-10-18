815e08e26c17194acae636b12b99f911ee357a05
Jgreen42 <creativemind47@outlook.com>
2023-10-17 21:54:22 -0700

    making a test deploy versus testing on machine

--- static/script.js
+++ static/script.js
@@ -1,34 +1,11 @@
-const video = document.getElementById('video-stream');
+// script.js
+const video = document.getElementById('webcam-feed');
 
-function startVideo() {
-    fetch('/video_feed')
-        .then(response => {
-            const reader = response.body.getReader();
-            const decoder = new TextDecoder('utf-8');
-
-            return new ReadableStream({
-                start(controller) {
-                    return pump();
-
-                    function pump() {
-                        return reader.read().then(({ done, value }) => {
-                            if (done) {
-                                controller.close();
-                                return;
-                            }
-                            controller.enqueue(value);
-                            return pump();
-                        });
-                    }
-                }
-            });
-        })
-        .then(stream => new Response(stream))
-        .then(response => response.blob())
-        .then(blob => {
-            const url = URL.createObjectURL(blob);
-            video.src = url;
-        });
-}
-
-startVideo();
+navigator.mediaDevices
+  .getUserMedia({ video: true })
+  .then((stream) => {
+    video.srcObject = stream;
+  })
+  .catch((error) => {
+    console.error('Error accessing webcam:', error);
+  });
